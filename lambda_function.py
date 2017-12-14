9b366ee1b16a389c94510cf42894666a5d5dc7bd
Theriley106 <christopherlambert106@gmail.com>
2017-12-14 16:54:04 -0500

    Finished alexa skill

--- lambda_function.py
+++ lambda_function.py
@@ -4,8 +4,52 @@ import random
 import re
 import csv
 
-STOCKS = ["JayNug", "Ayy Em Dee", "Tess a la", "En Vid Ia"]
-POSTITION = ["Short", "Long"]
+STOCKS = ["Jay Nug", "Ayy Em Dee", "Tesla", "Nvidia", "AMD", "Dry Ships Incorporated", 'turing pharmaceuticals', "Bitcoin", "any fucking cryptocurrency I don't care"]
+POSTITION = ["Short", "Long", "Puts", "Calls"]
+SPEND = ['a nintendo switch', 'an apartment to move out of your moms basement', 'a gaming computer', 'a girlfriend','a fucking yacht', 'a gym membership', 'cryptocurrency']
+JAILNAMES = ['in the slammer', 'in jail', 'in the joint', 'stuck in the jailhouse', 'locked up', 'in the clink bro', 'in the clink', ' locked up in the penitentiary']
+
+DD = ['doing some expert due diligence', 'getting my MBA from harvard university', 'rolling a dice', 'asking stocktwits', 'asking your mom', 'shaking a magic 8 ball', 'googling it', 'gaining some basic common sense', 'subscribing to wallstreetbets', 'making a thread on biz', 'cancelling my montley fool subscription']
+def adviceGen():
+	position = random.choice(POSTITION)
+	stock = random.choice(STOCKS)
+	if position == 'Short' or position == "Long":
+		text = 'you should {} {}'.format(position, stock)
+	else:
+		text = 'you should buy {} {}'.format(stock, position)
+	text = 'after {} i think that {}'.format(random.choice(DD), text)
+	if random.randint(1,2) == 2:
+		text = "{} and spend all your gains on {}".format(text, random.choice(SPEND))
+	return text
+
+
+
+def genName():
+	preFix = ['Your boy', "my man", 'that dude', 'investment god', 'ghost face killer', 'ma ma ma', 'trolling extraordinaire', "alleged stock god"]
+	firstName = ['Marvo', "Marvin", "Mart Can", "Marlin", "Martian", "Mirvin", "Martin"]
+	lastName = ['Shrell He', "Shrek He", "Shrek lee", "Shell E", "Shkreli", 'Shkay Rell E', ]
+	if random.randint(1, 2) == 1:
+		name = random.choice(preFix) + ", " + random.choice(firstName) + " " + random.choice(lastName)
+	else:
+		name = random.choice(firstName) + " " + random.choice(lastName)
+	return name
+
+def martinCheck():
+	if random.randint(1, 5) != 5:
+		text = "{} is still {}".format(genName(), random.choice(JAILNAMES))
+	else:
+		text = "on the advice of counsel I invoke my Fifth Amendment privilege against self-incrimination and respectfully decline to answer your question"
+	return {
+		"version": "1.0",
+		"sessionAttributes": {},
+		"response": {
+		"outputSpeech": {
+		"type": "PlainText",
+		"text": text
+			},
+			"shouldEndSession": False
+		  }
+		}
 
 def csvToList(fileName):
 	with open(fileName, 'rb') as f:
@@ -37,16 +81,42 @@ def returnTopComment(url):
 				pass
 	return parentComments
 
+def getAdvice():
+	text = adviceGen()
+	return {
+		"version": "1.0",
+		"sessionAttributes": {},
+		"response": {
+		"outputSpeech": {
+		"type": "PlainText",
+		"text": text
+			},
+			"shouldEndSession": False
+		  }
+		}
+
+def devInfo():
+	text = "created in December 2017 by Christopher Lambert.  This alexa skill is completely open sourced.  Please check out the skill on Gitub dot com slash theriley106 or by contacting me on reddit"
+	return {
+		"version": "1.0",
+		"sessionAttributes": {},
+		"response": {
+		"outputSpeech": {
+		"type": "PlainText",
+		"text": text
+			},
+			"shouldEndSession": False
+		  }
+		}
 
 def lambda_handler(event, context):
 	if event["request"]["type"] == "LaunchRequest":
 		return on_launch(event["request"], event["session"])
 	elif event["request"]["type"] == "IntentRequest":
 		return on_intent(event["request"], event["session"])
-	elif intent_name == "AMAZON.HelpIntent":
-		return get_welcome_response()
-	elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-		return handle_session_end_request()
+
+	else:
+		handle_session_end_request()
 
 def on_launch(launch_request, session):
 	return get_welcome_response()
@@ -54,10 +124,20 @@ def on_launch(launch_request, session):
 def on_intent(intent_request, session):
 	intent = intent_request["intent"]
 	intent_name = intent_request["intent"]["name"]
-	if intent_name == "getAdvice":
+	if intent_name == "whatMoves":
 		return returnComment()
+	elif intent_name == "martinCheck":
+		return martinCheck()
+	elif intent_name == "getAdvice":
+		return getAdvice()
+	elif intent_name == 'aboutDev':
+		return devInfo()
+	elif intent_name == "AMAZON.HelpIntent":
+		return get_welcome_response()
+	elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+		return handle_session_end_request()
 	else:
-		raise handle_session_end_request()
+		raise Exception("Done")
 
 def getTicker(word):
 	for ticker in STOCKS:
@@ -78,7 +158,7 @@ def getRandomComment():
 	message = f.read()
 	comments = message.split('\n')
 	f.close()
-	return random.choice(comments)
+	return str(random.choice(comments))
 
 
 
@@ -90,24 +170,29 @@ def returnComment():
 	"response": {
 	"outputSpeech": {
 	"type": "PlainText",
-	"text": getRandomComment()
+	"text": getRandomComment().replace('corporation', '').replace('incorporated', '').replace(', the', '')
 		},
 		"shouldEndSession": False
 	  }
 	}
 
 def handle_session_end_request():
-	card_title = "WallStreetBets"
-	speech_output = "I have no idea what you said, so I'm going to exit.  Thank you for asking an inanimate circular object how to invest your money"
-	should_end_session = True
-	return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
-
-
+	return {
+	"version": "1.0",
+	"sessionAttributes": {},
+	"response": {
+	"outputSpeech": {
+	"type": "PlainText",
+	"text": "I have no idea what you said, so I'm going to exit.  Thank you for asking an inanimate circular object how to invest your money"
+		},
+		"shouldEndSession": True
+	  }
+	}
 def get_welcome_response():
 	session_attributes = {}
 	card_title = "WallStreetBets"
 	speech_output = "Welcome to the are slash Wall Street Bets alexa skill. " \
-					" You can ask me for investment advice, or expert D D or whatever you want I don't care"
+					" You can ask me for investment advice, or expert due diligence or whatever you want."
 	reprompt_text = "Please ask me for advice or what my moves are in the coming days, " \
 					"I can answer a lot of the simple questions that are posted on to the wall street bets subreddit"
 	should_end_session = False
